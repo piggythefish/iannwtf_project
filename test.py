8a8f3dc87c30524b54ba80f342a848d33e3bf294
jensderkennts <p1ggyth3f1sh@gmail.com>
2023-03-01 15:00:57 +0100

    Create test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+
+def positional_encoding(length, d_model):
+    pos = np.arange(length)[:, np.newaxis]
+    i = np.arange(d_model)[np.newaxis, :]
+    angle_rates = 1 / np.power(10000, (2 * (i//2)) / np.float32(d_model))
+    angle_rads = pos * angle_rates
+    # apply sin to even indices in the array; 2i
+    angle_rads[:, 0::2] = np.sin(angle_rads[:, 0::2])
+    # apply cos to odd indices in the array; 2i+1
+    angle_rads[:, 1::2] = np.cos(angle_rads[:, 1::2])
+    pos_encoding = angle_rads[np.newaxis, ...]
+    return tf.cast(pos_encoding, dtype=tf.float32)
+
+pos_encoding = positional_encoding(50, 512)
+print (pos_encoding.shape)
+
+plt.pcolormesh(pos_encoding[0], cmap='RdBu')
+plt.xlabel('Depth')
+plt.xlim((0, 512))
+plt.ylabel('Position')
+plt.colorbar()
+plt.show()
